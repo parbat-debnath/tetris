08dcc56bff638e1536301b57d1b9f77888378ba7
parbat-debnath <parbardebnathofficial@gmail.com>
2025-12-24 18:01:33 +0530

    Fixed rendering bug, added collition detection feature

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ import * as util from './utils/logic.js'
 
 const gameArea = document.querySelector("#gamearea");
 
+
+gameArea.dataset.height = "24";
+gameArea.dataset.width = "12";
+
 util.loadMatrix(gameArea);
 
 let square = [{x : 0, y : 0}, {x : 0, y : 1}, {x : 1, y : 0}, {x : 1, y : 1}];
 
-util.startFall(square, gameArea);
+util.startFall(2,square, gameArea);
